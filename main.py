3a6fa694dc47d580a868403216e0fbd3bda39d75
apoorvaeswaramurthi <apoorvaeswaramurthi@uvic.ca>
2026-05-06 17:55:56 -0700

    addition of input validation for hours, priority, and productivity

--- main.py
+++ main.py
@@ -13,9 +13,33 @@ while True:
 
     if option == "1":
         task = input("Task: ")
-        hours = input("Hours: ")
-        priority = input("Priority (low/medium/high): ")
-        productivity = input("Productivity (1-5): ")
+
+        while True:
+            hours = input("Hours: ")
+            try:
+                float(hours)
+                break
+            except:
+                print("Invalid input. Please enter a number.")
+
+        while True:
+            priority = input("Priority (low/medium/high): ").lower()
+
+            if priority == "low" or priority == "medium" or priority == "high":
+                break
+            else:
+                print("Invalid input. Please enter low, medium, or high.")
+
+        while True:
+            productivity = input("Productivity (1-5): ")
+
+            if productivity.isdigit():
+                value = int(productivity)
+
+                if value >= 1 and value <= 5:
+                    break
+
+            print("Invalid input. Please enter an integer between 1 and 5.")
 
         tracker.add_entry(file_name, task, hours, priority, productivity)
     
